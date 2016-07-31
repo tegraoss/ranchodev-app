c01650a82e15d750b9afea84f812946d44134b54
Ana <ana.gabriel@tegra.me>
2016-07-31 11:58:17 -0300

    Create tabs

---
+++ app/pages/tabs/tabs.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/tabs/tabs.html',
+})
+export class TabsPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
