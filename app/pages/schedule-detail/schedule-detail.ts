26f555daab7d1f72c9ecbe7e650a40e34c939003
Ana <ana.gabriel@tegra.me>
2016-08-07 20:36:47 -0300

    Create page schedule-detail

---
+++ app/pages/schedule-detail/schedule-detail.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/schedule-detail/schedule-detail.html',
+})
+export class ScheduleDetailPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
