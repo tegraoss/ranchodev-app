dfeff07176407fc6ae43df15b8799521783f3476
Ana <ana.gabriel@tegra.me>
2016-08-07 20:12:56 -0300

    Include tabs to navigation

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Tabs, Tab } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+import { MapPage } from '../map/map';
+import { SchedulePage } from '../schedule/schedule'
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html',
 })
 export class TabsPage {
 
-  constructor(private nav: NavController) {
+  @ViewChild('scheduleTab') scheduleTab: Tab;
+  @ViewChild('aboutTab') aboutTab: Tab;
+  @ViewChild('mapTab') mapTab: Tab;
 
+  tab1Root: any 
+  tab2Root: any
+  tab3Root: any 
+  
+  constructor(navParams: NavParams) {
+    this.tab1Root = SchedulePage;
+    this.tab2Root = AboutPage;
+    this.tab3Root = MapPage;
   }
 
 }
+
