db21cbd423cc16bc74be73d6e3e85774c21b5583
Ana <ana.gabriel@tegra.me>
2016-08-07 20:16:04 -0300

    Consumes schedule-provider

--- app/pages/schedule/schedule.ts
+++ app/pages/schedule/schedule.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {TabsPage} from '../tabs/tabs';
+import {ScheduleProvider} from '../../provider/schedule-provider';
 
 @Component({
   templateUrl: 'build/pages/schedule/schedule.html',
 })
 export class SchedulePage {
 
-  constructor(private nav: NavController) {
+  schedule: Array<any> = [];
 
+  constructor(private nav: NavController, private scheduleProvider: ScheduleProvider) {
+  
+  }
+
+  ionViewLoaded()  {
+    this.loadSchedule();
+
+  }
+
+  loadSchedule() {
+    this.schedule = this.scheduleProvider.Obj.schedule;
   }
 
 }
